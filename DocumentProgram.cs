a40aa5b5ae83d9a8466fac1fb55f677ff6a31cd2
zohravali <valiyevazohra@gmail.com>
2022-01-21 12:20:27 +0400

    added InheritanceBackup

---
+++ DocumentProgram.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+  public  class DocumentProgram
+    {
+        public void OpenDocument()
+        {
+            Console.WriteLine("Document Opened\n");
+        }
+
+        internal void OpenDoucment(DocumentProgram docpro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Can Edit in Pro and Expert versions\n");
+        }
+
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Can Save in Pro and Expert versions\n");
+        }
+    }
+
+    class ProDocumentProgram : DocumentProgram
+    {
+        public sealed override void EditDocument()
+        {
+            Console.WriteLine("Document Edited\n"); 
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in doc format, for pdf format buy Expert packet\n");
+        }
+    }
+
+    class ExpertDocument : ProDocumentProgram
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in pdf format\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Delegate
+{
+    public delegate void DOCProgram();
+
+    public delegate void ProDOCProgram();
+
+    public delegate void ExpertProgram();
+    class Program
+    {
+        static void Start()
+        {
+            DocumentProgram docProgram = new();
+            DocumentProgram prodocProgram = new ProDocumentProgram();
+            DocumentProgram expDoc = new ExpertDocument();
+
+
+
+            DOCProgram program = docProgram.OpenDocument;
+            program += docProgram.EditDocument;
+            program += docProgram.SaveDocument;
+
+            ProDOCProgram proProg = prodocProgram.OpenDocument;
+            proProg += prodocProgram.EditDocument;
+            proProg += prodocProgram.SaveDocument;
+
+            ExpertProgram expProg = expDoc.OpenDocument;
+            expProg = expDoc.EditDocument;
+            expProg = expDoc.SaveDocument;
+
+            Console.WriteLine("Acar sozu daxil edin (basic/pro/expert): ");
+            string key = Console.ReadLine();
+
+            if (key == "basic")
+                program.Invoke();
+            else if (key == "pro")
+                proProg.Invoke();
+            else if (key == "expert")
+                expProg.Invoke();
+        }
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+    }
+}
